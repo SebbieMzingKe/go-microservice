5ae84b6948192dfc4e1ec4899f28f3bbf2912d05
SebbieMzingKe <sebbievilar2@gmail.com>
2024-12-27 23:56:20 +0300

    update folder structure

---
+++ orders/types.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"context"
+
+)
+
+type OrderService interface {
+	CreateOrder(context.Context) error
+}
+
+type OrdersStore interface {
+	Create(context.Context) error
+}
