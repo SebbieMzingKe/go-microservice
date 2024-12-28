aa7e66b7b40ea4389ea43b9e64a6ca8bd2c487c6
SebbieMzingKe <sebbievilar2@gmail.com>
2024-12-28 14:19:02 +0300

    feat: payload validation

--- gateway/main.go
+++ gateway/main.go
@@ -4,19 +4,34 @@ import (
 	"common"
 	"log"
 	"net/http"
+	pb "common/api"
 
 	_ "github.com/joho/godotenv/autoload"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
 	httpAddr = common.EnvString("HTTP_ADDR", ":3000")
+	ordersServiceAddr = "localhost:3000"
 )
 
 func main() {
+	conn, err := grpc.Dial(ordersServiceAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Failed to dial server: %v", err)
+	}
+
+	defer conn.Close()
+	log.Println("Dialing orders service at ", ordersServiceAddr)
+
+	c := pb.NewOrderServiceClient(conn)
+
 	mux := http.NewServeMux()
-	handler := NewHandler()
+	handler := NewHandler(c)
 	handler.registerRoutes(mux)
-	
+
 	log.Printf("Running on port %s", httpAddr)
 	if err := http.ListenAndServe(httpAddr, mux); err != nil {
 		log.Fatal("Failed to start http server")
